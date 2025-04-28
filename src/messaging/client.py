967f2ca55eb2ae0a59ccb34f84d32d9f8e7473a6
jpxch <orlando_alonzo@hotmail.com>
2025-04-28 00:04:53 -0700

    feat: live custom messaging enabled between client and server

--- src/messaging/client.py
+++ src/messaging/client.py
@@ -1,18 +1,27 @@
 import socket
 
-def send_message(message, host='127.0.0.1', port=5000):
+def send_message(host='127.0.0.1', port=5000):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         client_socket.connect((host, port))
+        print("[Connected to Soulwave Server]")
+
+        while True:
+            message = input ("You: ")
+            if message.lower() == 'exit':
+                print("[Disconnecting from server]")
+                break
+
         client_socket.send(message.encode())
 
         reply = client_socket.recv(1024).decode()
-        print(f"[Soulwave Server Replay] {reply}")
+        print(f"Soulwave: {reply}")
+
     except Exception as e:
         print(f"[Error] {e}")
     finally:
         client_socket.close()
 
 if __name__ == "__main__":
-    send_message("Hello from Gepeta!")
+    send_message()
